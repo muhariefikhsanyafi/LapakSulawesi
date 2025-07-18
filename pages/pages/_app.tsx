6194271003ea826ed516c4cf87e82ff3b24cc68f
Sahrul <86223518+awusahrul@users.noreply.github.com>
2025-07-19 00:00:57 +0800

    Delete pages/pages/_app.tsx

--- pages/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/awusahrul_desain_tampilan.css';
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-export default MyApp;
